7b1586fdb8c0408f21cbb0ec98730efd828d52ac
gc2321 <gchan.email@gmail.com>
2024-04-25 10:09:14 -0400

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,24 +1,10 @@
-using System.Xml.Serialization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-// builder.Services.AddControllers(opt =>
-// {
-//     var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-//     opt.Filters.Add(new AuthorizeFilter(policy));
-// });
-// builder.Services.AddApplicationServices(builder.Configuration);
-// builder.Services.AddIdentityServices(builder.Configuration);
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +12,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(
+        "CorsPolicy",
+        policy =>
+        {
+            policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+        }
+    );
 });
 
 var app = builder.Build();
@@ -37,13 +33,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
-
 try
 {
     var context = services.GetRequiredService<DataContext>();
@@ -55,5 +52,4 @@ catch (Exception ex)
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occured during migration");
 }
-
 app.Run();
