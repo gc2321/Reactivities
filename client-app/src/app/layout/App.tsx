2bac65351c5af6deb1477f23df973adc080a3cbf
gc2321 <gchan.email@gmail.com>
2024-04-29 14:27:39 -0400

    end of section 8 added routing

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,28 +1,23 @@
-import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
 import { observer } from 'mobx-react-lite';
-import { useStore } from '../stores/store';
-
+import HomePage from '../../features/activities/home/HomePage';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function App() {
-  const { activityStore } = useStore();
 
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
+  const location = useLocation();
 
   return (
     <>
-      <NavBar />
-      <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
-      </Container>
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <NavBar />
+          <Container style={{ marginTop: '7em' }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </>
   );
 }
